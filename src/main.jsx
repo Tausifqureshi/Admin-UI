2996f29f2c45817452c6ff3153fde4c06ef20e18
Tausifqureshi <tausifqureshi504@gmail.com>
2024-12-01 11:25:25 +0530

    Admin-UI

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import Recat from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Recat.StrictMode>
