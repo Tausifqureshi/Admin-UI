1d53263ced4d21d4251ae657ff61d475929f8c15
Tausifqureshi <tausifqureshi504@gmail.com>
2024-12-02 23:27:43 +0530

    femos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Table from "./components/Table";
+import Admin from "./components/Admin";
 
 const App = () => {
   return (
     <>
       <h1> Admin-UI</h1>
-      <Table />
+      <Admin />
     </>
   );
 };
