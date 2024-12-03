1c1d1453572af04cfa9feeacfec43ddd4b2c0aeb
Tausifqureshi <tausifqureshi504@gmail.com>
2024-12-03 09:41:01 +0530

    Admin-UI

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function Admin() {
-    
+    const [isData, setIsData] = useState(null);
+    const [error, setError] = useState(null);  // Error ke liye state
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setError(null); // Error reset karna
+                const response = await axios.get("https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json");
+                setIsData(response.data); // Data set karna
+            } catch (error) {
+                setError("Data fetch karne me error hua. Please try again."); // Error message set karna
+                console.error(err); // Log error for debugging
+            }
+        };
+
+        fetchData(); // Function call karna
+    }, []);
+
+    if (error) {
+     return <p style={{ color: "red", fontWeight: "bold" }}>{error}</p>
+    }
+
   return <div>
  
   </div>;
