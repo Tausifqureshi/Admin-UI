adac0561b71f0fdaf693f98bc1a397d76b8d3305
Tausifqureshi <tausifqureshi504@gmail.com>
2024-12-09 09:56:28 +0530

    Admin-UI

--- src/components/RowActions.jsx
+++ src/components/RowActions.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { FaEdit, FaSave, FaTrashAlt } from "react-icons/fa";
 
-const RowActions = ({ data,isEditing, onEditToggle, onDelete }) => {
+const RowActions = ({ isEditing, onEditToggle, onDelete }) => {
   return (
     <div>
       <button className={isEditing ? "save" : "edit"} onClick={onEditToggle}>
         {/* {isEditing ? "Save" : "Edit"} */}
         {isEditing ? <FaSave /> : <FaEdit />}
+       </button>
 
-      </button>
       <button className="delete" onClick={onDelete}>
        <FaTrashAlt />
       </button>
