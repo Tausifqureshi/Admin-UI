d4495db29260efbde8fb20f9c135f9bd5fbfa980
Tausifqureshi <tausifqureshi504@gmail.com>
2024-12-11 09:23:16 +0530

    Admin-UI

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Admin from "./components/Admin";
 const App = () => {
   return (
     <div className="App">
-      <h1> Admin-UI</h1>
+      <h1 className="bg-red-500 text-center underline"> Admin-UI</h1>
       <Admin />
     </div>
   );
