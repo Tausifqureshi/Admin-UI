091bd41121d505fe0e82b5f6a0000f47607e8426
Tausifqureshi <tausifqureshi504@gmail.com>
2024-12-19 18:11:14 +0530

    Admin

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Admin from "./components/Admin";
 const App = () => {
   return (
-    <>
+    <>  
       <Admin />
     </>
   );
-};
+}; 
 
 export default App;
